f3204cbccc8fb56acba8ea35cf26f93ae8aebd16
Diyar <diyarfranklin2000@gmail.com>
2024-11-28 13:24:32 +0100

    Typo fix

--- vAid-JavaFX/src/main/java/com/example/vaidjavafx/vAidApplication.java
+++ vAid-JavaFX/src/main/java/com/example/vaidjavafx/vAidApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class vAidApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(vAidApplication.class.getResource("settings.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(vAidApplication.class.getResource("dashboard-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
 
         // Add CSS stylesheet
